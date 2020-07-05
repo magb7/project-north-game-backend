5944e11e37db012e9736165842db10f2340f9ea5
sophie <sneuville59@gmail.com>
2020-07-05 18:36:41 +0200

    commit to pull modification

--- controllers/news.js
+++ controllers/news.js
@@ -1,23 +1,25 @@
 const { connection } = require("../conf");
 const getAllNews = async (req, res) => {
   const { author, title } = req.query;
-  let sqlRequest = `SELECT id, title, SUBSTR(content, 1, 100), picture_url as pictureUrl FROM news`;
-  if (author) {
-    console.log(author);
-    sqlRequest = ` WHERE author LIKE '%?%' `;
-  }
-  if (title) {
-    console.log(title);
-    sqlRequest += ` WHERE title LIKE '?%' `;
-  }
+
   try {
     // get all news or searchbar for the news
-    const [data] = await connection.query(sqlRequest, [author, title]);
+    const [data] = await connection.query([author, title]);
+
+    if (author) {
+      console.log(author);
+      `SELECT id, title, SUBSTR(content, 1, 100), picture_url as pictureUrl FROM news WHERE author LIKE '?%' `;
+    }
+    // if (title) {
+    //   console.log(title);
+    //   sqlRequest += ` WHERE title LIKE '?%' `;
+    // }
 
     return res.status(200).send(data);
   } catch (e) {
     console.log(e);
     console.log("author", author);
+
     return res.status(500).send("Error while reading the news.");
   }
 };
