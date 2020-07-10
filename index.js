01823986b52b41eadfcdbf622384123cf0a03901
magb7 <margaux.bedu@gmail.com>
2020-07-10 17:18:59 +0200

    add routes for authentication

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const app = express();
 
 const news = require("./routes/news.js");
 const events = require("./routes/events.js");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Router
 app.use("/news", news);
-
 app.use("/events", events);
+app.use("/auth", auth);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
