7efbb5e5afa479328db4e5f33b745bf9d7d73e13
magb7 <margaux.bedu@gmail.com>
2020-07-10 17:57:51 +0200

    change sql request for the creation of a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,28 +7,32 @@ const createUser = (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, 8);
 
   // Insertion in database
-  connection.query("INSERT INTO user SET ?", [req.body], (errReq, resReq) => {
-    if (errReq) {
-      console.log(errReq.sql);
-      console.log(errReq.message);
-      return res.status(500).send("Error while creating user");
+  connection.query(
+    "INSERT INTO user SET ? AND registration_date = NOW()",
+    [req.body],
+    (errReq, resReq) => {
+      if (errReq) {
+        console.log(errReq.sql);
+        console.log(errReq.message);
+        return res.status(500).send("Error while creating user");
+      }
+
+      // Creation of a user to make token
+      const user = {
+        mail: req.body.mail,
+        id: resReq.insertId,
+      };
+
+      // Creation of the token
+      const token = jwt.sign(user, `${tokenSecret}`);
+
+      // Sending back to the user the token and minor information
+      return res.status(200).send({
+        user,
+        token,
+      });
     }
-
-    // Creation of a user to make token
-    const user = {
-      mail: req.body.email,
-      id: resReq.insertId,
-    };
-
-    // Creation of the token
-    const token = jwt.sign(user, `${tokenSecret}`);
-
-    // Sending back to the user the token and minor information
-    return res.status(200).send({
-      user,
-      token,
-    });
-  });
+  );
 };
 
 module.exports = { createUser };
