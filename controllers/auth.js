c2cc6264514ccf9f6f324b543bff6defca1f0fa8
magb7 <margaux.bedu@gmail.com>
2020-07-10 17:39:42 +0200

    create functional controller for authentication

---
+++ controllers/auth.js
@@ -0,0 +1,34 @@
+const { connection, tokenSecret } = require("../conf");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const createUser = (req, res) => {
+  // Password encryption
+  req.body.password = bcrypt.hashSync(req.body.password, 8);
+
+  // Insertion in database
+  connection.query("INSERT INTO user SET ?", [req.body], (errReq, resReq) => {
+    if (errReq) {
+      console.log(errReq.sql);
+      console.log(errReq.message);
+      return res.status(500).send("Error while creating user");
+    }
+
+    // Creation of a user to make token
+    const user = {
+      mail: req.body.email,
+      id: resReq.insertId,
+    };
+
+    // Creation of the token
+    const token = jwt.sign(user, `${tokenSecret}`);
+
+    // Sending back to the user the token and minor information
+    return res.status(200).send({
+      user,
+      token,
+    });
+  });
+};
+
+module.exports = { createUser };
