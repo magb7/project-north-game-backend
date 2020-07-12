23dcd0726687f5aaa9a00802fec146f42464b20c
magb7 <margaux.bedu@gmail.com>
2020-07-12 15:46:46 +0200

    implement async await in createUser function

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,36 +2,36 @@ const { connection, tokenSecret } = require("../conf");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const createUser = (req, res) => {
-  // Password encryption
-  req.body.password = bcrypt.hashSync(req.body.password, 8);
+const createUser = async (req, res) => {
+  try {
+    // Password encryption
+    req.body.password = bcrypt.hashSync(req.body.password, 8);
 
-  // Insertion in database
-  connection.query(
-    `INSERT INTO user (name, mail, password, registration_date) VALUES ('${req.body.name}','${req.body.mail}','${req.body.password}', NOW())`,
-    (errReq, resReq) => {
-      if (errReq) {
-        console.log(errReq.sql);
-        console.log(errReq.message);
-        return res.status(500).send("Error while creating user");
-      }
+    // Insertion in database
+    const sqlRequest = `INSERT INTO user (name, mail, password, registration_date) VALUES ('${req.body.name}','${req.body.mail}','${req.body.password}', NOW())`;
 
-      // Creation of a user to make token
-      const user = {
-        mail: req.body.mail,
-        id: resReq.insertId,
-      };
+    const [resReq] = await connection.query(sqlRequest);
 
-      // Creation of the token
-      const token = jwt.sign(user, `${tokenSecret}`);
+    // Creation of a user to make token
+    const user = {
+      mail: req.body.mail,
+      id: resReq.insertId,
+    };
 
-      // Sending back to the user the token and minor information
-      return res.status(200).send({
-        user,
-        token,
-      });
-    }
-  );
+    // Creation of the token
+    const token = jwt.sign(user, `${tokenSecret}`);
+
+    // Sending back to the user the token and minor information
+    return res.status(200).send({
+      user,
+      token,
+    });
+  } catch (err) {
+    res.status(500).send("Error while creating user");
+    console.log(err.sql);
+    console.log(err.message);
+    return;
+  }
 };
 
 module.exports = { createUser };
