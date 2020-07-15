c883feb1dab67e2bce70cbb9fa8828f6f92fcded
sophie <sneuville59@gmail.com>
2020-07-15 14:12:20 +0200

    change the user_group table to insert constraint

--- database/schema.sql
+++ database/schema.sql
@@ -41,8 +41,13 @@ CREATE TABLE `group` (
   `max_players` int(11) NOT NULL,
   PRIMARY KEY (`id`));
 
-  CREATE TABLE `user_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`));
+ CREATE TABLE `user_group` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `group_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_group_user_idx` (`user_id`),
+  KEY `fk_user_group_group1_idx` (`group_id`),
+  CONSTRAINT `fk_user_group_group1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`),
+  CONSTRAINT `fk_user_group_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
