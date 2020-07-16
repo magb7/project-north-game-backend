8b927568f781d49d1a1e56053eef0b90533c2348
sophie <sneuville59@gmail.com>
2020-07-16 08:06:59 +0200

    addSQL request for search by maxplayers

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,22 +1,31 @@
 const { connection } = require("../conf");
 const getAllGroups = async (req, res) => {
   try {
-    let { authorId = "", name = "" } = req.query;
+    let { authorId = "", name = "", maxPlayer = "" } = req.query;
     let sqlRequest =
       "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group";
     if (authorId) {
       authorId = `${authorId}%`;
       sqlRequest =
-        "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group WHERE author_id = ? OR name LIKE ?";
+        "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group WHERE author_id = ? OR name LIKE ? OR max_players <= ?";
     }
     if (name) {
       name = `${name}%`;
       sqlRequest =
-        "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group WHERE author_id = ? OR name LIKE ?";
+        "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group WHERE author_id LIKE ? OR name LIKE ? OR max_players <= ?";
     }
+    if (maxPlayer) {
+      sqlRequest =
+        "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM northgame.group WHERE max_players <= ? OR name LIKE ? OR author_id LIKE ?;";
+    }
+
     // get all group or searchbar for the group
 
-    const [data] = await connection.query(sqlRequest, [authorId, name]);
+    const [data] = await connection.query(sqlRequest, [
+      authorId,
+      name,
+      maxPlayer,
+    ]);
     return res.status(200).send(data);
   } catch (e) {
     console.log(e);
@@ -24,6 +33,7 @@ const getAllGroups = async (req, res) => {
     return res.status(500).send("Error while reading the groups.");
   }
 };
+
 const getOneGroup = async (req, res) => {
   const { id } = req.params;
   try {
