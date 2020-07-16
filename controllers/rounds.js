51a0afb161395f9741dec1c704ac1c45c54c3d7c
narudb <benjam.d@gmail.com>
2020-07-16 19:32:01 +0200

    add all columns names

--- controllers/rounds.js
+++ controllers/rounds.js
@@ -7,12 +7,12 @@ const getAllRounds = async (req, res) => {
     if (name) {
       name = `${name}%`;
       sqlRequest =
-        'SELECT round.id AS roundId, round.name AS roundName, SUBSTR(content, 1, 100) as roundContent, image AS roundImage FROM round WHERE name LIKE ? OR place LIKE ?';
+        'SELECT round.id AS roundId, round.name AS roundName, SUBSTR(content, 1, 100) AS roundContent, image AS roundImage FROM round WHERE name LIKE ? OR place LIKE ?';
     }
     if (place) {
       place = `%${place}%`;
       sqlRequest =
-        'SELECT round.id AS roundId, round.name AS roundName, SUBSTR(content, 1, 100) as roundContent, image AS roundImage FROM round WHERE name LIKE ? OR place LIKE ?';
+        'SELECT round.id AS roundId, round.name AS roundName, SUBSTR(content, 1, 100) AS roundContent, image AS roundImage FROM round WHERE name LIKE ? OR place LIKE ?';
     }
     // get all games or searchbar for the rounds
 
@@ -31,7 +31,7 @@ const getOneRound = async (req, res) => {
     const [
       data,
     ] = await connection.query(
-      'SELECT round.id AS roundId, user.name AS roundCreator, round.name AS roundName, DATE_FORMAT(round_date, "%D %b %Y" ) AS roundDate, place AS roundPlace, user.name AS roundCreator, image AS roundImage FROM round JOIN user ON author_id = user.id WHERE round.id = ?',
+      'SELECT round.id AS roundId, user.name AS roundCreator, round.name AS roundName, content AS roundContent, DATE_FORMAT(round_date, "%D %b %Y" ) AS roundDate, DATE_FORMAT(round_date, "%H:%i" ) AS roundTime, DATE_FORMAT(creation_date, "%D %b %Y" ) AS creationDate, DATE_FORMAT(revision_date, "%D %b %Y" ) AS revisionDate, is_private, place AS roundPlace, image AS roundImage, avatar_url AS roundAvatar, max_players FROM round JOIN user ON author_id = user.id WHERE round.id = ?',
       [id]
     );
 
