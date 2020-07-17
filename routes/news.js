154a32a964a3d059a14a447b001825b5338b7ce4
magb7 <margaux.bedu@gmail.com>
2020-07-17 17:11:30 +0200

    move auth wall

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const newsController = require("../controllers/news");
 
@@ -8,6 +9,25 @@ router.get("/", newsController.getAllNews);
 
 // Get one news by the id
 router.get("/:id", newsController.getOneNews);
+
+// -------------------- Auth wall
+router.use((req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user, msg) => {
+    if (err) {
+      console.log("----");
+      console.log(err);
+      return res.status(500).send(err);
+    }
+    if (!user) {
+      console.log("----");
+      console.log("No user found");
+      return res.sendStatus(500);
+    }
+    //req.user = user;
+    next();
+  })(req, res);
+});
+// -------------------- / Auth wall
 
 // Post one news
 router.post("/", newsController.postNews);
