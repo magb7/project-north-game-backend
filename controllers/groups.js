234ea4f57a227cd9c3fce72ef5edc083baa6babb
magb7 <margaux.bedu@gmail.com>
2020-07-18 11:27:06 +0200

    add auth wall and resolve sql syntax error

--- controllers/groups.js
+++ controllers/groups.js
@@ -3,9 +3,9 @@ const getAllGroups = async (req, res) => {
   try {
     let { authorId = "", name = "", maxPlayer = "" } = req.query;
     let sqlRequest =
-      "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM group";
+      "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM `group`";
     const sqlRequestQuery =
-      "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM group WHERE author_id = ? OR name LIKE ? OR max_players <= ?";
+      "SELECT id, name, image, author_id as authorId, creation_date as creationDate, max_players as maxPLayer FROM `group` WHERE author_id = ? OR name LIKE ? OR max_players <= ?";
     if (authorId) {
       authorId = `${authorId}%`;
       sqlRequest = sqlRequestQuery;
@@ -40,7 +40,7 @@ const getOneGroup = async (req, res) => {
     const [
       data,
     ] = await connection.query(
-      "SELECT  group.id as groupId, group.name as groupName, group.image as groupImage, group.author_id as groupAuthor, group.creation_date as groupCreationDate, group.max_players as GroupMaxPlayers, COUNT(user.id) as numberOfPlayers FROM user_group JOIN user ON user.id=user_group.user_id JOIN group ON group.id=user_group.group_id WHERE group_id = ?",
+      "SELECT  group.id as groupId, group.name as groupName, group.image as groupImage, group.author_id as groupAuthor, group.creation_date as groupCreationDate, group.max_players as GroupMaxPlayers, COUNT(user.id) as numberOfPlayers FROM user_group JOIN user ON user.id=user_group.user_id JOIN `group` ON group.id=user_group.group_id WHERE group_id = ?",
       [id]
     );
 
